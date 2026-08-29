87b091ecfa91a72cc776043babb0c4636c946204
R1cardo4lexandre <ricardoalexandre7700@gmail.com>
2026-08-29 18:05:35 -0300

    feat: adição da entidade EvolutionLine e registro no admin

--- digimon/admin.py
+++ digimon/admin.py
@@ -1,3 +1,4 @@
 from django.contrib import admin
+from .models import EvolutionLine
 
-# Register your models here.
+admin.site.register(EvolutionLine)
--- digimon/models.py
+++ digimon/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class EvolutionLine(models.Model):
+    
+    #Relacionamento com a entidade User
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    name = models.CharField(max_length=50)
+    
+    #Fases evolutivas dos digimons representadas em colunas da tabela
+    baby_i_id = models.PositiveIntegerField(null=True, blank=True)
+    baby_ii_id = models.PositiveIntegerField(null=True, blank=True)
+    child_id = models.PositiveIntegerField(null=True, blank=True)
+    adult_id = models.PositiveIntegerField(null=True, blank=True)
+    perfect_id = models.PositiveIntegerField(null=True, blank=True)
+    ultimate_id = models.PositiveIntegerField(null=True, blank=True)
+    super_ultimate_id = models.PositiveIntegerField(null=True, blank=True)
+    
+    def __str__(self):
+        return EvolutionLine.name
